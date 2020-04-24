8826fa64db609a625c79d8a11dc68b5c995109da
pconesa <p.conesa.mingo@gmail.com>
2020-04-24 16:46:31 +0200

    add resources to setup.py

--- setup.py
+++ setup.py
@@ -180,9 +180,9 @@ setup(
     # If using Python 2.6 or earlier, then these have to be included in
     # MANIFEST.in as well.
     # include_package_data=True,
-    # package_data={  # Optional
-    #   'relion': ['relion_logo.gif', 'protocols.conf'],
-    # },
+    package_data={
+      'pyworkflow':['resources/*']
+    },
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
