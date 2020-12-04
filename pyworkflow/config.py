d2d846c26fccee0dc7b442392022b219e2c2c261
pconesa <p.conesa.mingo@gmail.com>
2020-12-04 14:03:53 +0100

    Module created for Exceptions PyworkflowExceptions has an URL ShowError, understand PyworkflowExceptions and shows url an traceback Constants:  DOCSITEURL class with URLS for exceptions  SCIPION_SCRATCH as constant PATTERN_LINK considers # in urls
    makeTmpPath:
     Launches a PyworkflowException
     Project used in folderId

--- pyworkflow/config.py
+++ pyworkflow/config.py
@@ -115,7 +115,7 @@ class Config:
     # Path for Scipion logs
     SCIPION_LOGS = _get('SCIPION_LOGS', _join(SCIPION_USER_DATA, 'logs'))
     # Scratch path
-    SCIPION_SCRATCH = _get('SCIPION_SCRATCH', None)
+    SCIPION_SCRATCH = _get(SCIPION_SCRATCH, None)
 
     # Get general log file path
     SCIPION_LOG = _join(SCIPION_LOGS, 'scipion.log')
