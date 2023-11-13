1a426e012d5a1c49a44744c26534adc03f70e3d8
Grigory Sharov <sharov.grigory@gmail.com>
2023-11-13 15:45:23 +0000

    update actions, remove <3.8 support

--- pyworkflow/plugin.py
+++ pyworkflow/plugin.py
@@ -37,10 +37,7 @@ logger = logging.getLogger(__name__)
 import glob
 import os
 import importlib
-try:
-    from importlib import metadata
-except ImportError:  # for Python<3.8
-    import importlib_metadata as metadata
+from importlib import metadata
 import inspect
 import traceback
 import types
