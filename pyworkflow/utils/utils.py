8981a3aac92e6d4853c3989bf98f6cda63004620
Marta Martinez <mmmtnez@cnb.csic.es>
2025-11-14 16:05:34 +0100

    Fixed typo

--- pyworkflow/utils/utils.py
+++ pyworkflow/utils/utils.py
@@ -36,8 +36,7 @@ import sysconfig
 import bibtexparser
 import numpy as np
 import math
-from pyworkflow.constants import StrColors,
-TRUE_YES_ON_
+from pyworkflow.constants import StrColors, TRUE_YES_ON_
 from pyworkflow import Config
 
 
@@ -708,7 +707,7 @@ def envVarOn(varName, env=None):
 def strToBoolean(string):
     """ Converts a string into a Boolean if the string is on of true, yes, on, 1. Case insensitive."""
     return string is not None and string.lower() in TRUE_YES_ON_
-    
+
 def strToDuration(durationStr):
     """ Converts a string representing an elapsed time to seconds
     E.g.: for "1m 10s" it'll return  70 """
